52e6f4b4db04803a721445058d8286078a650b17
shad-k <shadab.2005@gmail.com>
2017-04-20 12:09:07 +0530

    Add Google Places Library and put markers

--- src/js/app.js
+++ src/js/app.js
@@ -4,12 +4,39 @@ document.querySelector(".arrow").addEventListener("click", function(event) {
 	document.querySelector(".arrow").classList.toggle("floatRight");
 });
 
+var markers = [];
+var map;
+
 // Callback function that runs when the Google Maps API has loaded successfully
 function initMap() {
+	var pos = new google.maps.LatLng(28.5714851, 77.2591487);
 	// The coordinates below belong to New Delhi
-	var map = new google.maps.Map(document.getElementById("map"), {
+	map = new google.maps.Map(document.getElementById("map"), {
 		zoom: 15,
-		center: {lat: 28.5714851, lng: 77.2591487},
+		center: pos,
 		mapTypeControl: false // Disabling the map type controls since it is not needed currently
 	});
+
+
+	// Places Service
+
+	var places = new google.maps.places.PlacesService(map);
+
+	places.nearbySearch({
+		location: pos,
+		radius: "2000",
+		rankBy: google.maps.places.RankBy.PROMINENCE
+	}, function(results, status) {
+		makeMarkers(results);
+	});
+}
+
+function makeMarkers(results) {
+	results.forEach(function(result) {
+		var marker = new google.maps.Marker({
+			position: result.geometry.location,
+			map: map
+		});
+		markers.push(marker);
+	})
 }
